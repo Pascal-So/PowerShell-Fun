6377aa3e188238eec1ee41f8912a4be485e0cbc1
Pascal Sommer <p@pascalsommer.ch>
2020-05-27 23:00:04 +0200

    add interval formatter

---
+++ Format-ShortInterval.ps1
@@ -0,0 +1,20 @@
+Function Format-ShortInterval([Parameter(Mandatory)] [TimeSpan] $Interval) {
+    <#
+    .SYNOPSIS
+        Pretty print short time spans
+
+    .EXAMPLE
+        Format-ShortInterval("00:03:42")
+        1m 42s
+    #>
+
+    if ($Interval.TotalMinutes -lt 1.0) {
+        return "{0:f2}s" -f $Interval.TotalSeconds
+    }
+
+    if ($Interval.TotalHours -lt 1.0) {
+        return "{0}m {1:d2}s" -f $Interval.Minutes, $Interval.Seconds
+    }
+
+    return "{0}h {1:d2}m {2:d2}s" -f [math]::floor($Interval.TotalHours), $Interval.Minutes, $Interval.Seconds
+}
